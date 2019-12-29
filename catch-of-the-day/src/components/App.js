668377c7a2506898f8d741e6c9aec1c563848af9
Jackson Kaufman <jacksonkaufman115@gmail.com>
2019-12-28 22:20:03 -0700

    Added state changes and store picker component

--- catch-of-the-day/src/components/App.js
+++ catch-of-the-day/src/components/App.js
@@ -4,6 +4,18 @@ import Order from "./Order";
 import Inventory from "./Inventory";
 
 class App extends React.Component {
+  state = {
+    fishes: {},
+    order: {}
+  };
+  addFish = (fish) => {
+    // take a copy of the existing state
+    const fishes = { ...this.state.fishes };
+    // Add our new fish to that fishes variable
+    fishes[`fish${Date.now()}`] = fish;
+    // Set the new fishes object to state
+    this.setState({ fishes });
+  };
   render() {
     return (
       <>
@@ -12,7 +24,7 @@ class App extends React.Component {
           <Header tagline="Fresh Seafood Market"/>
         </div>
         <Order />
-        <Inventory />
+        <Inventory addFish={this.addFish}/>
       </div>
       </>
     )
